253be81e3f08d43f41011b2a9e267eb86c7a748b
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-03-07 15:02:29 +0100

    refactor(): add beg of gen architecture

--- generator/src/maze_generating/rect_generating/generate_rect_maze.c
+++ generator/src/maze_generating/rect_generating/generate_rect_maze.c
@@ -8,12 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gen_rect_gen_head.h"
+#include "gen_link_list_head.h"
 
 int generate_rect_maze(int ac, char **av)
 {
     m_maze *maze = create_struct_maze(ac, av);
+    m_list *pos = NULL;
 
-    for (int i = 0; maze->maze[i] != NULL; i++)
-        printf("%s\n", maze->maze[i]);
+    init_linked_list(0, 0, &pos);
     return (0);
 }
