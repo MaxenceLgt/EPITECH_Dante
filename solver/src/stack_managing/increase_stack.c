7c3b5b0c835f2a4a01dee4c836b532084937e71c
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-03-08 11:21:27 +0100

    refactor(): Add stack managing

--- solver/src/stack_managing/increase_stack.c
+++ solver/src/stack_managing/increase_stack.c
@@ -9,14 +9,13 @@
 
 void increase_stack(void)
 {
-    __u_long size = 256L * 1024L * 1024L;
     struct rlimit r_lim;
 
     if (getrlimit(RLIMIT_STACK, &r_lim)){
         return;
     }
-    if (r_lim.rlim_cur < size){
-        r_lim.rlim_cur = size;
+    if (r_lim.rlim_cur < r_lim.rlim_max){
+        r_lim.rlim_cur = r_lim.rlim_max;
         setrlimit(RLIMIT_STACK, &r_lim);
         return;
     }
